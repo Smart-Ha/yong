c6fcf8f05877bbd09fbee6b78954989f0503fdf9
wangyong <wangyong@proudsmart.com>
2018-07-20 19:33:52 +0800

    add sql

--- src/main/java/com/wangy/config/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.wangy.config;
-
-public class Constants {
-
-    public static final String CURRENT_USERNAME = "CURRENT_USERNAME";
-    public static final String ONLY_CLEAR_CACHE = "ONLY_CLEAR_CACHE";
-}
---
+++ src/main/java/com/wangy/constant/Constants.java
@@ -0,0 +1,26 @@
+package com.wangy.core.domain;
+
+public class Constants {
+
+    /**
+     * 上个页面地址
+     */
+    public static final String BACK_URL = "BackURL";
+
+    public static final String IGNORE_BACK_URL = "ignoreBackURL";
+
+    /**
+     * 当前请求的地址 带参数
+     */
+    public static final String CURRENT_URL = "currentURL";
+
+    ///////////////////////用户相关 shiro //////////////////////
+    /**
+     *当前用户名
+     */
+    public static final String CURRENT_USERNAME = "CURRENT_USERNAME";
+    /**
+     *
+     */
+    public static final String ONLY_CLEAR_CACHE = "ONLY_CLEAR_CACHE";
+}
